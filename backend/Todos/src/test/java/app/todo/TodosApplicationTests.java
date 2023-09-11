4596c3efcf0e62329878cad4afdac086baa59442
TalelMejri <talelmejri8@gmail.com>
2023-09-11 17:24:37 +0100

    firstCommit

---
+++ backend/Todos/src/test/java/app/todo/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package app.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_selector_macos
+import location
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+}
