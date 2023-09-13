f214aeb144700352358e0cbdc078fc4eec67e520
TalelMejri <talelmejri8@gmail.com>
2023-09-13 12:48:54 +0100

    add TodoComponents

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import * as Aos from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    Aos.init()
+  }
   title = 'Angular';
 }
---
+++ Angular/src/app/todo/todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent {
+
+}
