807c7097a4a1336edfb0f7c446a74b4ecb07ca95
TalelMejri <talelmejri8@gmail.com>
2023-09-13 14:45:03 +0100

    disabled

--- Angular/src/app/todo/card-todo/card-todo.component.ts
+++ Angular/src/app/todo/card-todo/card-todo.component.ts
@@ -1,6 +1,6 @@
 import { Component,Input } from '@angular/core';
-import { Todo } from 'src/app/Model/todo';
-
+import { TodoServiceService } from 'src/app/services/todo-service.service';
+import {MatSnackBar} from "@angular/material/snack-bar"
 @Component({
   selector: 'app-card-todo',
   templateUrl: './card-todo.component.html',
@@ -8,4 +8,18 @@ import { Todo } from 'src/app/Model/todo';
 })
 export class CardTodoComponent {
   @Input() public item:any;
+  constructor(private todoServ:TodoServiceService,private MatSnackBar:MatSnackBar){
+
+  }
+  changeStatus(id:number){
+    this.todoServ.ChangerStatus(id).subscribe((res)=>{
+      this.MatSnackBar.open("Status Changed",'close',{
+        duration:3000
+      })
+    },((error)=>{
+      this.MatSnackBar.open("Status Changed",'close',{
+        duration:3000
+      })
+    }));
+   }
 }
