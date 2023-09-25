f60b04a8054e9125e919059e342e02a46170a105
TalelMejri <talelmejri8@gmail.com>
2023-09-25 17:14:11 +0100

    SecurityConfig

--- backend/Todos/src/main/java/app/todo/SpringSecurity/UserDetailsImpl.java
+++ backend/Todos/src/main/java/app/todo/SpringSecurity/UserDetailsImpl.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import app.todo.Reposiory.UserRepo;
 import app.todo.entity.User;
 
+
+@Service
 public class UserDetailsImpl implements UserDetailsService {
 
 	@Autowired
