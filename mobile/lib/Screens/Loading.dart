d09db876ef0a3215ef2ebe8bc015d08d6e97e0ca
TalelMejri <talelmejri8@gmail.com>
2023-09-27 22:44:37 +0100

    change loading

--- mobile/lib/Screens/Loading.dart
+++ mobile/lib/Screens/Loading.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Services/AuthService.dart';
 
-class Loading extends StatelessWidget {
+
+class Loading extends StatefulWidget {
   const Loading({super.key});
 
   @override
+  State<Loading> createState() => _Loading();
+}
+
+class _Loading extends State<Loading> {
+  AuthService authService=AuthService();
+  String nom="";
+  @override
+  void initState() {
+       authService.getUserFromStorage().then((value) =>
+       setState((){
+          nom=authService.isAuth ? authService.user!.Nom : '';
+       })
+       );
+     super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(child: Text("Hello ",style: TextStyle(color: Colors.black,fontSize: 25),),)
+      body: Center(child: Text("Hello ${nom}",style: const TextStyle(color: Colors.black,fontSize: 25),),)
     );
   }
 }
